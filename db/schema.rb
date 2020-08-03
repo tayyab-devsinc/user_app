800b442352c258e1a9f8f13ed569801d6b15672c
tayyab-devsinc <muhammad.tayyab@devsinc.com>
2020-08-03 19:05:37 +0500

    Implemented cookies to remember user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200730142902) do
+ActiveRecord::Schema.define(version: 20200803133303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200730142902) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
